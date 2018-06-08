57237d8dabe385bf54a7570b18c720c7546d67e3
rjandson1 <ruslan.avtomatov@gmail.com>
2018-06-08 11:52:42 -0700

    Update 2

--- app/src/main/java/com/example/android/musicplayerapp/Rock.java
+++ app/src/main/java/com/example/android/musicplayerapp/Rock.java
@@ -19,9 +19,18 @@ public class Rock extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
 
-        ArrayList<Song> songs = new ArrayList<>();
+        final ArrayList<Song> songs = new ArrayList<>();
         songs.add(new Song("Radiohead","Paranoid Android"));
-        songs.add(new Song("Radiohead","Chickens"));
+        songs.add(new Song("Metallica","Nothing Else Matters"));
+        songs.add(new Song("Aerosmith","Dream On"));
+        songs.add(new Song("Def Leppard","Love Bites"));
+        songs.add(new Song("The Rolling Stones","Paint It Black"));
+        songs.add(new Song("Led Zeppelin","Stairway to Heaven"));
+        songs.add(new Song("Deep Purple","Child In Time"));
+        songs.add(new Song("Free","All Right Now"));
+        songs.add(new Song("Nirvana","Smells Like Teen Spirit"));
+        songs.add(new Song("Eagles","Hotel California"));
+
 
         SongAdapter adapter = new SongAdapter ( this, songs);
 
